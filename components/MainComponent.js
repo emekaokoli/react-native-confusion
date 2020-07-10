a0a086c53e4115633db062df313804ba76625797
Emeka Okoli <emekaokoli00@gmail.com>
2020-07-10 23:35:17 +0100

    updated files

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React, { Component } from 'react'
-import Dishdetail from './DishdetailComponent.js'
+import Dishdetail from './DishdetailComponent'
 import Menu from './MenuComponent'
 
 
@@ -38,42 +37,3 @@ export default class Main extends Component {
     )
   }
 }
-=======
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { DISHES } from '../shared/dishes'
-import Dishdetail from './DIshdetailComponent'
-import Menu from './MenuComponent'
-
-export default class Main extends Component {
-                 constructor(props) {
-                   super(props)
-
-                   this.state = {
-                     dishes: DISHES,
-                     selectedDish: null,
-                   }
-                 }
-                 onDishSelect(dishId) {
-                   this.setState({ selectedDish: dishId })
-                 }
-
-                 render() {
-                   return (
-                     <View style={{ flex: 1 }}>
-                       <Menu
-                         dishes={this.state.dishes}
-                         onPress={(dishId) => this.onDishSelect(dishId)}
-                       />
-                       <Dishdetail
-                         dish={
-                           this.state.dishes.filter(
-                             (dish) => dish.id === this.state.selectedDish,
-                           )[0]
-                         }
-                       />
-                     </View>
-                   )
-                 }
-               }
->>>>>>> d171971752561053eb5ad775b617074cbb837a35
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
@@ -24,7 +23,6 @@ render() {
        title={item.name}
        subtitle={item.description}
        hideChevron={true}
-       //  onPress={() => navigate('Dishdetail')}
        onPress={() =>
          this.props.navigation.navigate({
            name: 'Dishdetail',
@@ -51,33 +49,3 @@ render() {
   }
 }
 export default Menu
-=======
-import React from 'react'
-import { FlatList } from 'react-native'
-import { ListItem } from 'react-native-elements'
-
-export default function Menu(props) {
-  const renderMenuItem = ({ item, index }) => {
-    return (
-      <ListItem
-        key={index}
-        title={item.name}
-        subtitle={item.description}
-        hideChevron={true}
-        onPress={() => props.onPress(item.id)}
-        leftAvatar={{ source: require('./images/uthappizza.png') }}
-      />
-    )
-  }
-
-  return (
-    <>
-      <FlatList
-        data={props.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </>
-  )
-}
->>>>>>> d171971752561053eb5ad775b617074cbb837a35
