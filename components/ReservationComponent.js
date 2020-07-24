c08e6d7107538f57630b17e85e171cb336617b50
Emeka Okoli <emekaokoli00@gmail.com>
2020-07-24 19:08:46 +0100

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,3 +1,5 @@
+import { Notifications } from 'expo'
+import * as Permissions from 'expo-permissions'
 import React, { Component } from 'react'
 import {
   Alert,
@@ -43,12 +45,38 @@ class Reservation extends Component {
       showModal: false,
     })
   }
+ async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
 
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
   Resevationlert = () => {
     const formatDate = (string) => {
       var options = { year: 'numeric', month: 'long', day: 'numeric' }
       return new Date(string).toLocaleDateString([], options)
     }
+   
     Alert.alert(
       'Is your Reservation Okay',
       `Number of Guests: ${this.state.guests},
@@ -60,7 +88,7 @@ class Reservation extends Component {
           onPress: () => this.resetForm(),
           style: 'cancel',
         },
-        { text: 'OK', onPress: () => this.resetForm() },
+        { text: 'OK', onPress: () =>  {this.presentLocalNotification(this.state.date);this.resetForm() }},
       ],
       { cancelable: false },
     )
